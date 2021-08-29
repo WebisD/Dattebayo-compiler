78d708810d05a8ec722dd15559ff580fe4778924
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-08-29 17:19:59 -0300

    chore(): change interface
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

--- interfaces/LexerRulesInterface.py
+++ interfaces/LexerRulesInterface.py
@@ -1,12 +1,12 @@
 import abc
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 class LexerRulesInterface(metaclass=abc.ABCMeta):
     """A LexerRules interface that will be used for lexer rules class creation.
     """
     def __init__(self):
-        self.reserved_words: List[str] = []
+        self.reserved_words: Dict[str, str] = {}
         self.identifiers: List[str] = []
         self.operators: List[str] = []
 
@@ -14,7 +14,7 @@ class LexerRulesInterface(metaclass=abc.ABCMeta):
     def __subclasshook__(cls, subclass):
         return (
                 hasattr(subclass, 'reserved_words')
-                and hasattr(subclass, 'reserved_words')
-                and hasattr(subclass, 'reserved_words')
+                and hasattr(subclass, 'identifiers')
+                and hasattr(subclass, 'operators')
                 or NotImplemented
         )
