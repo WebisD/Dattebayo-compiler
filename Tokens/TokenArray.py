b1b761883fe892ddba7302d17e03750a1985ff77
Antonio Muniz <antonio.gustavo_@live.com>
2021-09-15 22:28:57 -0300

    Added dict of tokens

--- Tokens/TokenArray.py
+++ Tokens/TokenArray.py
@@ -3,16 +3,32 @@ from Tokens.TokenEnum import TokenEnum
 
 
 class TokenArray(object):
-  def __init__(self, array_tokens):
-    self.array_tokens = array_tokens
+  def __init__(self, dict_tokens):
+    self.dict_tokens = dict_tokens
     self.add_tokens()
 
   def add_tokens(self):
-    ninjutsu = Token(TokenEnum.NINJUTSU, "ninjutsu", "^(ninjutsu)$")
-    genjutsu = Token(TokenEnum.GENJUTSU, "genjutsu", "^(genjutsu)$")
-    taijutsu = Token(TokenEnum.TAIJUTSU, "taijutsu", "^(taijutsu)$")
+    self.dict_tokens['ninjutsu'] = Token(TokenEnum.NINJUTSU, "ninjutsu", "^(ninjutsu)$")
+    self.dict_tokens['genjutsu'] = Token(TokenEnum.GENJUTSU, "genjutsu", "^(genjutsu)$")
+    self.dict_tokens['taijutsu'] = Token(TokenEnum.TAIJUTSU, "taijutsu", "^(taijutsu)$")
+    self.dict_tokens['kagebunshin'] = Token(TokenEnum.KAGEBUNSHIN, "kagebunshin", "^(kagebunshin)$")
+    self.dict_tokens['tsukuyomi'] = Token(TokenEnum.TSUKUYOMI, "tsukuyomi", "^(tsukuyomi)$")
+    self.dict_tokens['chakra'] = Token(TokenEnum.CHAKRA, "chakra", "^(chakra)$")
+    self.dict_tokens['kamui'] = Token(TokenEnum.KAMUI, "kamui", "^(kamui)$")
+    self.dict_tokens['sharingan'] = Token(TokenEnum.SHARINGAN, "sharingan", "^(sharingan)$")
+    self.dict_tokens['rasengan'] = Token(TokenEnum.RASENGAN, "rasengan", "^(rasengan)$")
+    self.dict_tokens['raikiri'] = Token(TokenEnum.RAIKIRI, "raikiri", "^(raikiri)$")
+    self.dict_tokens['zetsu'] = Token(TokenEnum.ZETSU, "zetsu", "^(zetsu)$")
+    self.dict_tokens['kuchiyose'] = Token(TokenEnum.KUCHIYOSE, "kuchiyose", "^(kuchiyose)$")
+    self.dict_tokens['fuumashuriken'] = Token(TokenEnum.FUUMASHURIKEN, "fuumashuriken", "^(fuumashuriken)$")
+    self.dict_tokens['kunai'] = Token(TokenEnum.KUNAI, "kunai", "^(kunai)$")
+    self.dict_tokens['shuriken'] = Token(TokenEnum.SHURIKEN, "shuriken", "^(shuriken)$")
+    self.dict_tokens['katana'] = Token(TokenEnum.KATANA, "katana", "^(katana)$")
+    self.dict_tokens['kirigakure'] = Token(TokenEnum.KIRIGAKURE, "kirigakure", "^(kirigakure)$")
+    self.dict_tokens['sunagakure'] = Token(TokenEnum.SUNAGAKURE, "sunagakure", "^(sunagakure)$")
+    self.dict_tokens['konohagakure'] = Token(TokenEnum.KONOHAGAKURE, "konohagakure", "^(konohagakure)$")
+    self.dict_tokens['kumogakure'] = Token(TokenEnum.KUMOGAKURE, "kumogakure", "^(kumogakure)$")
+    self.dict_tokens['amegakure'] = Token(TokenEnum.AMEGAKURE, "amegakure", "^(amegakure)$")
 
-    self.array_tokens.append(ninjutsu)
-    self.array_tokens.append(genjutsu)
-    self.array_tokens.append(taijutsu)
+
 
