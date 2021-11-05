f9b2ec77a76ed5136e9432d7d46da75ea73a6ee7
WebisD <weverson.webis@gmail.com>
2021-11-05 12:15:38 -0300

    Lexer match floats

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -64,6 +64,20 @@ class Lexer:
     def replace_word(self, pattern: str, replacement: str, target: str):
         return re.sub(pattern, replacement, target)
 
+    def find_longest_match(self):
+        max_match = [0, None]
+        k_index = -1
+
+        for k, v in self.token_array.dict_tokens.items():
+            match = self.check_word(self.token_array.dict_tokens[k].regex, self.current_text)
+            if match:
+                num_match = match.end() - match.start()
+                if num_match > max_match[0]:
+                    max_match = [num_match, match]
+                    k_index = k
+
+        return max_match[1], k_index
+
     def get_tokens(self) -> List[Token]:
         if not self.is_reading:
             return []
@@ -76,19 +90,18 @@ class Lexer:
             continue_loop = False
             # print(f"current text: {self.current_text}")
 
-            for k, v in self.token_array.dict_tokens.items():
-                match = self.check_word(self.token_array.dict_tokens[k].regex, self.current_text)
-                if match:
-                    # print(f"\tinside if: {self.token_array.dict_tokens[k].type}")
-                    if self.token_array.dict_tokens[k].type == TokenEnum.INTEGER:
-                        tokens.append(Token(self.token_array.dict_tokens[k].type, int(match[0])))
-                    elif self.token_array.dict_tokens[k].type == TokenEnum.FLOAT:
-                        tokens.append(Token(self.token_array.dict_tokens[k].type, float(match[0])))
-                    else:
-                        tokens.append(Token(self.token_array.dict_tokens[k].type, match[0]))
-                    self.current_text = self.replace_word(self.token_array.dict_tokens[k].regex, '', self.current_text)
-                    continue_loop = True
-                    break
+            match, k = self.find_longest_match()
+            if match:
+                aaa = match[0]
+                # print(f"\tinside if: {self.token_array.dict_tokens[k].type}")
+                if self.token_array.dict_tokens[k].type == TokenEnum.INTEGER:
+                    tokens.append(Token(self.token_array.dict_tokens[k].type, int(match[0])))
+                elif self.token_array.dict_tokens[k].type == TokenEnum.FLOAT:
+                    tokens.append(Token(self.token_array.dict_tokens[k].type, float(match[0])))
+                else:
+                    tokens.append(Token(self.token_array.dict_tokens[k].type, match[0]))
+                self.current_text = self.replace_word(self.token_array.dict_tokens[k].regex, '', self.current_text)
+                continue_loop = True
 
             if continue_loop:
                 continue
